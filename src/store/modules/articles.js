7c02b29bbb12858fe3175b99072fe23f6331324d
Squeak-Paradox <olliemchurch@gmail.com>
2022-05-29 09:13:57 +0100

    refactors data into modules

---
+++ src/store/modules/articles.js
@@ -0,0 +1,22 @@
+export default {
+    state: {
+        featured: [
+            {
+                name: 'From Actor to Software Developer',
+                linkTo: '#'
+            },
+            {
+                name: 'Why I Love CSS Grid',
+                linkTo: '#'
+            },
+            {
+                name: 'Transferable Skills',
+                linkTo: '#'
+            },
+            {
+                name: 'Build Tic Tac Toe',
+                linkTo: '#'
+            }
+        ]
+    }
+}
