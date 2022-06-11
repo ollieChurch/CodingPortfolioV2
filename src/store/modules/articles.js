87df5a785c99c56df334d18a705e0d47eacfc92a
Squeak-Paradox <olliemchurch@gmail.com>
2022-06-11 16:29:23 +0100

    add article links

--- src/store/modules/articles.js
+++ src/store/modules/articles.js
@@ -7,16 +7,12 @@ export default {
                 linkTo: 'https://olliechurch.notion.site/From-Actor-To-Software-Developer-5d29d41a4c9c42c9b9659041ad63852b'
             },
             {
-                name: 'I ❤️ CSS Grid',
-                linkTo: '#'
+                name: '3 Rules For 5 Star Customer Service',
+                linkTo: 'https://olliechurch.notion.site/3-Rules-For-5-Star-Customer-Service-81d3f01b516241d6a037bb0cadb1d752'
             },
             {
-                name: 'Nothing Wasted',
-                linkTo: '#'
-            },
-            {
-                name: 'Build Tic Tac Toe',
-                linkTo: '#'
+                name: 'A Day In The Life Of A Software Dev',
+                linkTo: 'https://olliechurch.notion.site/A-Day-In-The-Life-Of-A-Software-Dev-fbb8607d7417498fa8f11e80f98b3e0b'
             }
         ]
     }
