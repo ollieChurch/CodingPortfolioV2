381c1e7f6eac6667e71fcff990665e4a26adf7d4
Squeak-Paradox <olliemchurch@gmail.com>
2022-06-16 22:45:29 +0100

    temp removes actor to software dev article

--- src/store/modules/articles.js
+++ src/store/modules/articles.js
@@ -2,10 +2,10 @@ export default {
     state: {
         homeLink: 'https://olliechurch.notion.site/My-Articles-659858523e6b4437b9010d9736d9f51a',
         featured: [
-            {
-                name: 'Actor to Software Developer',
-                linkTo: 'https://olliechurch.notion.site/From-Actor-To-Software-Developer-5d29d41a4c9c42c9b9659041ad63852b'
-            },
+            // {
+            //     name: 'Actor to Software Developer',
+            //     linkTo: 'https://olliechurch.notion.site/From-Actor-To-Software-Developer-5d29d41a4c9c42c9b9659041ad63852b'
+            // },
             {
                 name: '3 Rules For 5 Star Customer Service',
                 linkTo: 'https://olliechurch.notion.site/3-Rules-For-5-Star-Customer-Service-81d3f01b516241d6a037bb0cadb1d752'
