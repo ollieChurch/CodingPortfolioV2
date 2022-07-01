92db320bbdec79841014e7cee21c2571962d5588
Squeak-Paradox <olliemchurch@gmail.com>
2022-07-01 12:27:54 +0100

    adds scrimba articles

--- src/store/modules/articles.js
+++ src/store/modules/articles.js
@@ -2,10 +2,14 @@ export default {
     state: {
         homeLink: 'https://olliechurch.notion.site/My-Articles-659858523e6b4437b9010d9736d9f51a',
         featured: [
-            // {
-            //     name: 'Actor to Software Developer',
-            //     linkTo: 'https://olliechurch.notion.site/From-Actor-To-Software-Developer-5d29d41a4c9c42c9b9659041ad63852b'
-            // },
+            {
+                name: 'SCRIMBA BLOG - Actor To Software Dev',
+                linkTo: 'https://scrimba.com/articles/from-actor-to-web-developer/'
+            },
+            {
+                name: '🎧 SCRIMBA PODCAST 🎧 Do the Projects You Find Fun!',
+                linktTo: 'https://scrimba.com/podcast/do-the-projects-you-find-fun-and-take-time-to-process-rejection-actor-turned-developer-ollie-church/' 
+            },
             {
                 name: '3 Rules For 5 Star Customer Service',
                 linkTo: 'https://olliechurch.notion.site/3-Rules-For-5-Star-Customer-Service-81d3f01b516241d6a037bb0cadb1d752'
