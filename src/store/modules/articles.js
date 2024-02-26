232da9055c2d397f787fbc5c06d81e4a179d0eef
Squeak-Paradox <olliemchurch@gmail.com>
2024-02-26 14:05:11 +0000

    Adds new article

--- src/store/modules/articles.js
+++ src/store/modules/articles.js
@@ -1,23 +1,23 @@
 export default {
     state: {
         homeLink: 'https://olliechurch.notion.site/My-Articles-659858523e6b4437b9010d9736d9f51a',
-        featured: [
-            {
-                name: 'SCRIMBA BLOG - Actor To Software Dev',
-                linkTo: 'https://scrimba.com/articles/from-actor-to-web-developer/'
-            },
-            {
+		featured: [
+			{
                 name: '🎧 SCRIMBA PODCAST 🎧 Do the Projects You Find Fun!',
-                linktTo: 'https://scrimba.com/podcast/do-the-projects-you-find-fun-and-take-time-to-process-rejection-actor-turned-developer-ollie-church/' 
+                linkTo: 'https://scrimba.com/podcast/do-the-projects-you-find-fun-and-take-time-to-process-rejection-actor-turned-developer-ollie-church/' 
             },
+			{
+				name: 'Defending Hybrid Amidst the Office Tug-of-War',
+				linkTo: 'https://olliechurch.notion.site/Defending-Hybrid-Amidst-the-Office-Tug-of-War-c396c4539b8643f3be70307d59192b6d'
+			},
             {
                 name: '3 Rules For 5 Star Customer Service',
                 linkTo: 'https://olliechurch.notion.site/3-Rules-For-5-Star-Customer-Service-81d3f01b516241d6a037bb0cadb1d752'
+			},
+			{
+                name: 'SCRIMBA BLOG - Actor To Software Dev',
+                linkTo: 'https://scrimba.com/articles/from-actor-to-web-developer/'
             },
-            {
-                name: 'A Day In The Life Of A Junior Dev',
-                linkTo: 'https://olliechurch.notion.site/A-Day-In-The-Life-Of-A-Software-Dev-fbb8607d7417498fa8f11e80f98b3e0b'
-            }
         ]
     }
 }
